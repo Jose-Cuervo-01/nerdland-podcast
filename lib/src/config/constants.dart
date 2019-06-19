049e4c3edd6aaca59e523941791ceca9ebc28b82
devrnt <devrient.jonas@gmail.com>
2019-06-19 11:58:51 +0200

    Add podcast service

---
+++ lib/src/config/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static String debugBackendUrl = 'http://192.168.0.229';
+  // TODO: Deploy the backend on Heroku and insert the url
+  static String productionBackendUrl = '';
+
+  static int backendPort = 3000;
+}
---
+++ lib/src/services/podcast_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert' show json;
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:nerdland_podcast/src/config/constants.dart';
+
+import 'package:nerdland_podcast/src/models/podcast.dart';
+
+class PodcastService {
+  final http.Client client;
+  String url;
+  int port;
+
+  PodcastService({@required this.client}) {
+    url = Constants.debugBackendUrl;
+    port = Constants.backendPort;
+  }
+
+  Future<List<Podcast>> fetchPodcasts() async {
+    final response = await client.get('$url:$port/podcasts');
+    Future<List<Podcast>> podcastsFuture;
+    if (response.statusCode == 200) {
+      podcastsFuture = compute(parsePodcasts, response.body);
+    } else {
+      throw Exception(
+          'Failed to fetch podcasts, statuscode: ${response.statusCode}');
+    }
+    return podcastsFuture;
+  }
+}
+
+List<Podcast> parsePodcasts(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Podcast>((json) => Podcast.fromJson(json)).toList();
+}
