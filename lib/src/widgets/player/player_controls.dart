75064b5bc27fd00d12ef70ffe5eb315682ad6c81
devrnt <devrient.jonas@gmail.com>
2019-06-22 16:10:04 +0200

    Add player controls * Contains the progresslider, timestamp, title, button

---
+++ lib/src/widgets/player/player_controls.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:nerdland_podcast/src/widgets/marquee_widget.dart';
+import 'package:nerdland_podcast/src/widgets/player/player_button.dart';
+import 'package:nerdland_podcast/src/widgets/player/player_control_title.dart';
+import 'package:nerdland_podcast/src/widgets/player/player_time_stamp.dart';
+import 'package:nerdland_podcast/src/widgets/player/progress_slider.dart';
+
+class PlayerControls extends StatefulWidget {
+  @override
+  _PlayerControlsState createState() => _PlayerControlsState();
+}
+
+class _PlayerControlsState extends State<PlayerControls> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Flexible(
+          flex: 1,
+          child: ProgressSlider(),
+        ),
+        Flexible(
+          flex: 9,
+          child: playerControls,
+        ),
+      ],
+    );
+  }
+
+  Widget get playerControls {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Flexible(
+                flex: 3,
+                child: PlayerTimeStamp(),
+              ),
+              Flexible(
+                flex: 6,
+                child: MarqueeWidget(
+                  direction: Axis.horizontal,
+                  child: PlayerControlTitle(),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: PlayerButton(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
