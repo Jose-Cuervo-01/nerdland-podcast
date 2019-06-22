1f0b74fcf82fd63c0bab54245283e7e139d13aea
devrnt <devrient.jonas@gmail.com>
2019-06-22 16:13:40 +0200

    Add progress slider

---
+++ lib/src/widgets/player/progress_slider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:nerdland_podcast/src/blocs/player_control_bloc.dart';
+import 'package:provider/provider.dart';
+
+class ProgressSlider extends StatefulWidget {
+  @override
+  _ProgressSliderState createState() => _ProgressSliderState();
+}
+
+class _ProgressSliderState extends State<ProgressSlider> {
+  PlayerControlBloc _playerControlBloc;
+
+  @override
+  void didChangeDependencies() {
+    _playerControlBloc = Provider.of<PlayerControlBloc>(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _playerControlBloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    PlayerControlBloc playerState = Provider.of<PlayerControlBloc>(context);
+
+    return StreamBuilder(
+      stream: _playerControlBloc.playStatus$,
+      builder: (context, AsyncSnapshot<PlayStatus> snapshot) {
+        if (snapshot.hasData) {
+          return Slider(
+            min: 0.0,
+            max: 1.0,
+            value: snapshot.data.currentPosition / snapshot.data.duration,
+            onChanged: (sliderValue) {
+              int timeToSnapTo = (snapshot.data.duration * sliderValue).toInt();
+              playerState.seek(timeToSnapTo);
+            },
+          );
+        } else {
+          return Slider(
+            value: 0.0,
+            onChanged: null,
+          );
+        }
+      },
+    );
+  }
+}
