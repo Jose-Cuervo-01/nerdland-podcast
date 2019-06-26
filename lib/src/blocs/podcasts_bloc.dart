db01f27b15673b953360f7ebeefdebdbfeaeefbc
devrnt <devrient.jonas@gmail.com>
2019-06-26 20:36:39 +0200

    Add previous and next podcast to podcasts bloc

--- lib/src/blocs/podcasts_bloc.dart
+++ lib/src/blocs/podcasts_bloc.dart
@@ -12,7 +12,6 @@ class PodcastsBloc {
   PodcastService _podcastService = PodcastService(client: http.Client());
 
   BehaviorSubject<List<Podcast>> _podcastsController = BehaviorSubject();
-
   Stream<List<Podcast>> get podcasts$ => _podcastsController.stream;
 
   PodcastsBloc() {
@@ -22,6 +21,22 @@ class PodcastsBloc {
   void fetchPodcasts() async {
     _podcasts = await _podcastService.fetchPodcasts();
     _podcastsController.add(_podcasts);
+  }
+
+  Podcast previousPodcast(Podcast currentPodcast) {
+    int indexCurrent =
+        _podcasts.indexWhere((podcast) => currentPodcast.guid == podcast.guid);
+
+    int indexPrevious = indexCurrent == 0 ? indexCurrent : indexCurrent - 1;
+    return _podcasts[indexPrevious];
+  }
+
+  Podcast nextPodcast(Podcast currentPodcast) {
+    int indexCurrent =
+        _podcasts.indexWhere((podcast) => currentPodcast.guid == podcast.guid);
+    int indexNext =
+        indexCurrent == _podcasts.length - 1 ? indexCurrent : indexCurrent + 1;
+    return _podcasts[indexNext];
   }
 
   void dispose() {
